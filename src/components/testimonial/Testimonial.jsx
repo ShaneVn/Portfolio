fd6c963ed025adf931c3d22fe7c75a6ec3c9d1db
Shane <shaneboo152@gmail.com>
2021-10-20 22:57:15 -0700

    development

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -7,11 +7,33 @@ export default function Testimonial() {
       <div className="container">
         <div className="card">
           <div className="top">
-              <img src="/image/right-arrow.png" className="left" alt="left-arrow" />
-              <img src="/image/Apple_logo_grey.png" alt="apple-logo" />
+            <img 
+              src="/image/right-arrow.png"
+              className="left"
+              alt="left-arrow"
+            />
+            <img 
+            className = "logo"
+            src="/image/Apple_logo_grey.png" alt="apple-logo" />
           </div>
-          <div className="center"></div>
-          <div className="bottom"></div>
+          <div className="center">
+            <h3>Metadata Curator</h3>
+            <h4>Apple</h4>
+            
+          </div>
+          <div className="bottom">
+            <p class="des">
+              {" "}
+              • Verify content accuracy, ensuring it meets the provided
+              standards <br />
+              <br />
+              • Make edits, fixes, or suggestions to the data content when
+              needed
+              <br />
+              <br />• Report any patterns of issues observed to help improve
+              database and product performance
+            </p>
+          </div>
         </div>
       </div>
     </div>
