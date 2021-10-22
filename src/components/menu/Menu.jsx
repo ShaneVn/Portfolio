1b542359caf2e74514275dcbd74c9db7bb98fce3
Shane <shaneboo152@gmail.com>
2021-10-21 17:42:57 -0700

    development

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -9,13 +9,13 @@ export default function Menu({menuOpen, setMenuOpen}) {
                     <a href="#intro">Home</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
-                    <a href="#portfolio">Porfolio</a>
+                    <a href="#works">About</a>
                 </li >
                 <li onClick={()=>setMenuOpen(false)}>
-                    <a href="#works">Works</a>
+                    <a href="#portfolio">Projects</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
-                    <a href="#testimonials">Testimonials</a>
+                    <a href="#testimonials">History</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,41 +1,86 @@
 import "./testimonial.scss";
 
 export default function Testimonial() {
+  const data = [
+  
+
+{
+    id: 1,
+  company: "Apple",
+  title: "Metadata Curator",
+  logo:
+  "/image/Apple_logo_grey.png",
+  icon: "/image/right-arrow.png",
+  lineOne: "• Verify content accuracy, ensuring it meets the provided standards",
+  lineTwo: " • Make edits, fixes, or suggestions to the data content when needed",
+  lineThree: "• Report any patterns of issues observed to help improve database and product performance",
+  
+  },
+
+  {  id: 2,
+    company: "Tesla",
+    title: "Data Annotation Specialist",
+    logo:
+    "/image/Tesla.png",
+    icon: "/image/right-arrow.png",
+    lineOne: "•  Interact and assist Autopilot Engineers to provide high quality data",
+    lineTwo: " • Work with a user interface for image and data labeling",
+    lineThree: "• Collaborate with the annotation team to ensure that annotations meet project requirements and deliver high-quality data under deadline",
+    feature: true,
+    },
+
+{
+    id: 3,
+    company: "San Jose State University",
+    title: "Business Management",
+    logo:
+    "/image/SJSU.png",
+    icon: "/image/right-arrow.png",
+    lineOne:" Bachelor of Science",
+    lineTwo: "Graduated Fall 2018",
+    
+}
+  ]
+
+
   return (
+    
     <div className="testimonials" id="testimonials">
-      <h1>Work History & Education</h1>
+      <h1>Work & Education</h1>
       <div className="container">
-        <div className="card">
+      { data.map((d)=> (
+        <div className={d.feature? "card feature" : "card"} key={d.id} >
           <div className="top">
-            <img 
-              src="/image/right-arrow.png"
+            {/* <img 
+              src= {d.icon}
               className="left"
               alt="left-arrow"
-            />
+            /> */}
             <img 
             className = "logo"
-            src="/image/Apple_logo_grey.png" alt="apple-logo" />
+            src={d.logo}alt="apple-logo" />
           </div>
           <div className="center">
-            <h3>Metadata Curator</h3>
-            <h4>Apple</h4>
+            <h3>{d.title}</h3>
+            <h4>{d.company}</h4>
             
           </div>
           <div className="bottom">
             <p class="des">
               {" "}
-              • Verify content accuracy, ensuring it meets the provided
-              standards <br />
+              {d.lineOne} <br />
               <br />
-              • Make edits, fixes, or suggestions to the data content when
-              needed
+              {d.lineTwo}
               <br />
-              <br />• Report any patterns of issues observed to help improve
-              database and product performance
+              <br /> {d.lineThree}
             </p>
           </div>
         </div>
+      ))}
       </div>
     </div>
   );
+
+  
+    
 }
