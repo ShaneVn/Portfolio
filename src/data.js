d23939953169b0618a490f40d574dd7cfb35da7e
Shane <shaneboo152@gmail.com>
2021-11-17 19:08:01 -0800

    add projects

--- src/data.js
+++ src/data.js
@@ -1,93 +1,77 @@
-export const reactProject =[
-    {
+export const reactProject = [
+  {
     id: 1,
     title: "Neflix Clone",
     img: "/image/netflix-clone.jpg",
-    link:"https://shanevn.github.io/react-netflix-clone/"
-}, 
-    {
-    id:2,
+    link: "https://shanevn.github.io/react-netflix-clone/",
+  },
+  {
+    id: 2,
     title: "Mock E-commerce",
-    img:"/image/E-commerce.png",
-    link:"http://ShaneVn.github.io/react-small-ecommerce-mock-website"
-},
-    {
-        id:3,
-        title:"Todo App",
-        img:"/image/Todo.png",
-        link:"http://ShaneVn.github.io/react-todo-app"
-    }, 
+    img: "/image/E-commerce.png",
+    link: "http://ShaneVn.github.io/react-small-ecommerce-mock-website",
+  },
+  {
+    id: 3,
+    title: "Todo App",
+    img: "/image/Todo.png",
+    link: "http://ShaneVn.github.io/react-todo-app",
+  },
 
-    {
-        id: 4,
-        title: "Social Media App",
-        img: "/image/Social.PNG",
-        link:"https://shanevn.github.io/react-netflix-clone/"
-    }, 
+  {
+    id: 4,
+    title: "Social Media App",
+    img: "/image/Social.PNG",
+    link: "https://github.com/ShaneVn/mern-social-media",
+  },
 
-    {
-        id: 5,
-        title: "Travel Page",
-        img: "/image/Product.PNG",
-        link:"https://shane-travel-page.netlify.app/"
-    }, 
+  {
+    id: 5,
+    title: "Travel Page",
+    img: "/image/Product.PNG",
+    link: "https://shane-travel-page.netlify.app/",
+  },
 
-    {
-        id: 6,
-        title: "Movie Search app",
-        img: "/image/movie.png",
-        link:"http://ShaneVn.github.io/react-movie-search-app"
-    }, 
+  {
+    id: 6,
+    title: "Movie Search app",
+    img: "/image/movie.png",
+    link: "http://ShaneVn.github.io/react-movie-search-app",
+  },
+];
 
+export const javaScriptProject = [
+  {
+    id: 1,
+    title: "personal-dashboard",
+    link: "https://shanevn.github.io/vanilla-personal-dashboard/",
+    img: "/image/dashboard.png",
+  },
 
-]
-
-export const javaScriptProject=[
-    {
-        id:1 ,
-        title: "War",
-        img:"/image/war-game.png",
-        link:""
-    } ,
-    
-    {
-        id:2 ,
-        title: "Pac-Man Game",
-        img:"/image/pac-man.png",
-        link:"https://shanevn.github.io/vanilla-pac-man/"
-    } ,
-    
-    {
-        id:3,
-        title:"personal-dashboard",
-        link:"https://shanevn.github.io/vanilla-personal-dashboard/",
-        img:"/image/dashboard.png"
-    }
-]
-
-
+  {
+    id: 2,
+    title: "Pac-Man Game",
+    img: "/image/pac-man.png",
+    link: "https://shanevn.github.io/vanilla-pac-man/",
+  },
+];
 
 export const typeScriptProject = [
-    {
-        id:1,
-        title:"react-with-typescript",
-        link:"https://shane-react-with-typescript.netlify.app/",
-        img:"/image/typescript.PNG"
-
-
-    }
-]
-
+  {
+    id: 1,
+    title: "react-with-typescript",
+    link: "https://shane-react-with-typescript.netlify.app/",
+    img: "/image/typescript.PNG",
+  },
+];
 
 export const nodeProject = [
-    {
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "/image/Social.PNG",
+    link: "https://github.com/ShaneVn/mern-social-media",
+  },
+];
 
-
-}]
-  
-
-export const cssProject = [
-    {
-        
-    }
-]
+export const cssProject = [{}];
